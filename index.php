c7c94bac45df46d8eed756b0258cabdf9e989495
HenriqueGNR <108287012+HenriqueGNR@users.noreply.github.com>
2025-06-19 00:24:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include('layouts/header.php'); ?>
+<?php include('https://raw.githubusercontent.com/HenriqueGNR/signos-zodiaco/refs/heads/main/layouts/header.php'); ?>
 
 <div class="container mt-5">
     <div class="row justify-content-center">
